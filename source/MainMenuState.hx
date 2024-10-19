75da25ecc9479b96739ac3a0a4fe87988c0c93b4
erico0203 <160540627+erico0203@users.noreply.github.com>
2024-10-19 14:23:02 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -33,12 +33,12 @@ class MainMenuState extends MusicBeatState
 	private var camAchievement:FlxCamera;
 	
 	var optionShit:Array<String> = [
-		'story_mode',
+		
 		'freeplay',
 		#if desktop 'mods', #end
-		#if ACHIEVEMENTS_ALLOWED 'awards', #end
-		'credits',
-		#if !switch 'donate', #end
+		#if ACHIEVEMENTS_ALLOWED  #end
+		
+		#if !switch  #end
 		'options'
 	];
 
